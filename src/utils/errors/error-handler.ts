4a0cf6e9099ed17d18ce4c3dcb76fcbdb43ff386
K-KAD <54041472+k-kaddal@users.noreply.github.com>
2024-06-04 17:49:23 +0100

    feat(errors): implementing error handler, error classes and unit tests (#2)
    * feat(errors): implementing error handler, error classes and unit tests
    
    * refactor(errors): grouping all api errors

---
+++ src/utils/errors/error-handler.ts
@@ -0,0 +1,35 @@
+// todo(handleError): replace with error logging
+// todo(handleError): implement retry mechanism
+
+/* eslint-disable no-console */
+import { APIError } from './api-error';
+import { CustomError } from './custom-error';
+import { HttpStatusCode } from './http-status-code';
+
+export const handleError = (error: Error): void => {
+  if (error instanceof APIError) {
+    if (error.statusCode === HttpStatusCode.BAD_REQUEST) {
+      console.error(`BadRequest (${error.statusCode}): ${error.message}`);
+    } else if (error.statusCode === HttpStatusCode.NOT_FOUND) {
+      console.error(`NotFound error (${error.statusCode}): ${error.message}`);
+    } else if (error.statusCode === HttpStatusCode.FORBIDDEN) {
+      console.error(`Forbiden error (${error.statusCode}): ${error.message}`);
+    } else if (error.statusCode === HttpStatusCode.UNAUTHORIZED) {
+      console.error(
+        `Unauthorized error (${error.statusCode}): ${error.message}`,
+      );
+    } else if (
+      error.statusCode >= HttpStatusCode.INTERNAL_SERVER_ERROR &&
+      error.statusCode < 600
+    ) {
+      console.error(`Internal Error ${error.statusCode}: ${error.message}`);
+      console.error('Retrying ...');
+    }
+  } else if (error instanceof CustomError) {
+    console.error('Custom error:', error.message);
+  } else {
+    console.error('An unexpected error occurred:', error.message);
+  }
+
+  throw error;
+};
---
+++ src/utils/errors/index.ts
@@ -0,0 +1,4 @@
+export * from './custom-error';
+export * from './api-error';
+export * from './error-handler';
+export * from './http-status-code';
