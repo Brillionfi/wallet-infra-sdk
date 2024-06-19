cf61e0dee1c30c56e60b7ca417cacc213fba9dfb
k-kaddal <khaledkaddal@gmail.com>
2024-06-19 11:58:31 +0100

    docs(token): adding documentations for getTokens and getTokenById

--- src/wallet-infra.ts
+++ src/wallet-infra.ts
@@ -1,12 +1,15 @@
 import { WalletService } from '@services/wallet.service';
 import { TransactionService } from '@services/transaction.service';
+import { TokenService } from '@services/token.service';
 
 export class WalletInfra {
   public Transaction: TransactionService;
   public Wallet: WalletService;
+  public Token: TokenService;
 
   constructor() {
     this.Transaction = new TransactionService();
     this.Wallet = new WalletService();
+    this.Token = new TokenService();
   }
 }
