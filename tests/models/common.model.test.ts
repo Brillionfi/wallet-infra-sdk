8fb54b9320deaae02ce202275e48d4dce00ac7f3
santiago-ab <santiago.martinez@brillion.finance>
2024-06-24 11:04:05 -0400

    fix(): conflicts

--- tests/models/common.model.test.ts
+++ tests/models/common.model.test.ts
@@ -1,9 +1,5 @@
-<<<<<<< feature/BRIL-528-sign-a-transaction
 import { EthereumAddressSchema, non0xString } from '@models/common.models';
-=======
 import { AuthProvider } from '@models/auth.models';
-import { EthereumAddressSchema } from '@models/common.models';
->>>>>>> main
 
 describe('Common Schemas', () => {
   it('should throw an error if address is not valid', () => {
@@ -17,14 +13,13 @@ describe('Common Schemas', () => {
     expect(data).toBeDefined();
   });
 
-<<<<<<< feature/BRIL-528-sign-a-transaction
   it('should return valid non0xString', () => {
     const example = 'e6d0c561728eFeA5EEFbCdF0A5d0C945e3697bEA';
     const data = non0xString.parse(example);
     expect(data).toBeDefined();
-=======
+  });
+
   it('Should have the correct casing for login methods', () => {
     expect(AuthProvider.GOOGLE).toBe('Google');
->>>>>>> main
   });
 });
