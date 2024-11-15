98849d20496d5c109d5d6c9720cae3ff175099c2
k-kaddal <khaledkaddal@gmail.com>
2024-11-15 23:48:59 +0000

    feat(): adding indexer for transaction history

--- src/api/wallet.api.ts
+++ src/api/wallet.api.ts
@@ -185,12 +185,13 @@ export class WalletApi {
   public async getTransactionHistory(
     address: Address,
     chainId: ChainId,
+    indexer: 'internal' | 'external',
     page = 0,
   ): Promise<{ transactions: Partial<ITransaction>[]; currentPage: number }> {
     logger.debug(`${this.className}: Getting Wallets`);
     try {
       const response: AxiosResponse = await this.httpClient.get(
-        `/wallets/${address}/chains/${chainId}/transactions?page=${page}`,
+        `/wallets/${address}/chains/${chainId}/transactions?page=${page}&&indexer=${indexer}`,
       );
 
       const transactions = TransactionSchema.partial()
--- src/services/wallet.service.ts
+++ src/services/wallet.service.ts
@@ -107,11 +107,17 @@ export class WalletService {
   public async getTransactionHistory(
     address: Address,
     chainId: ChainId,
+    indexer: 'internal' | 'external',
     page = 0,
   ): Promise<{ transactions: Partial<ITransaction>[]; currentPage: number }> {
     logger.info(`${this.className}: Getting Wallet transaction history`);
     try {
-      return await this.walletApi.getTransactionHistory(address, chainId, page);
+      return await this.walletApi.getTransactionHistory(
+        address,
+        chainId,
+        indexer ? indexer : 'internal',
+        page,
+      );
     } catch (error) {
       throw handleError(error);
     }
